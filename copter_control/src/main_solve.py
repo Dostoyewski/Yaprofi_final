4c6e467fb3c4b6098241583f7467258dc250e8ce
Dostoyewski <dostoyewski@yandex.ru>
2020-02-29 11:33:51 +0300

    Added params callback

--- copter_control/src/main_solve.py
+++ copter_control/src/main_solve.py
@@ -21,12 +21,12 @@ class DroneController:
     def __init__(self):
 
         "ROS stuff"
-        self.marker_features_sub = rospy.Subscriber('/marker/features', CameraFeatures, self.marker_features_callback)
-        self.drone_odom_sub = rospy.Subscriber('/drone/odom', Odometry, self.drone_odom_callback)
+        self.drone_odom_sub = rospy.Subscriber('/tello/odom', Odometry, self.drone_odom_callback)
         self.drone_target_pose_pub = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=1, latch=True)
         self.drone_takeoff_pub = rospy.Publisher('/tello/takeoff', Empty, queue_size=1, latch=True)
         self.drone_estop_pub = rospy.Publisher('/tello/emergency', Empty, queue_size=1, latch=True)
         self.drone_land_pub = rospy.Publisher('/tello/land', Empty, queue_size=1, latch=True)
+        self.marker_features_sub = rospy.Subscriber('/start/params', CopterTarget, self.fly_params_callback)
 
         "Drone state"
         self.drone_state_position = Point()
@@ -34,8 +34,36 @@ class DroneController:
         self.drone_state_lin_vel = Vector3()
         self.drone_state_ang_vel = Vector3()
 
+        "Fly params"
+        self.h = 0
+        self.dh = 0
+        self.phi = 0
+
+        "fly etape"
+        """ 0 — взлет, 1 — набор высоты, 2 — задержка, 3 — изменение высоты,
+        4 — задержка, 5 — изменение ориентации, 6 — задержка, 7 — посадка"""
+        self.code = -1
+
         "Camera features for feedback"
         self.camera_features = CameraFeatures()
+
+    def fly_params_callback(self, msg):
+        """
+        Fly params callback
+        Sets h, dh, phi
+        """
+        lock.acquire()
+
+        self.h = msg.h
+        self.dh = msg.dh
+        self.phi = msg.phi
+
+        "Starting process"
+        self.code = 0
+
+        print("Fly parametes:", self.h, self.dh, self.phi)
+
+        lock.release()
         
 
     def drone_odom_callback(self, msg):
@@ -56,43 +84,28 @@ class DroneController:
 
         lock.release()
 
-    def marker_features_callback(self, msg):
-        """
-        Features extraction callback.
-        Gets features ()
-        """
-        lock.acquire()
-        self.camera_features = msg
-        lock.release()
-
     def stop(self):
         """ Reset the robot """
         pose = Pose()
         pose.position.x = 0
         pose.position.y = 0
-        pose.position.z = self.Z0
+        pose.position.z = 0
         self.drone_target_pose_pub.publish(pose)
-
-        self.marker_features_sub.unregister()
         self.drone_odom_sub.unregister()
         self.drone_target_pose_pub.unregister()
 
     def update(self, dt, t):
         """ Update control signal for drone """
-        ####################################################
-        #### TODO WRITE YOUR SOLUTION BELOW! ###############
-        #### THIS IS DRONE CONTROLLER ######################
-        #### THIS CODE JUST FOR EXAMPLE ####################
-        ####################################################
 
-        self.pos_x = 0
-        self.pos_y = 0
+        if self.code != -1:
+            self.pos_x = 0
+            self.pos_y = 0
 
-                
-        pose = Pose()
-        pose.position.x = self.pos_x
-        pose.position.y = self.pos_y
-        pose.position.z = self.Z0   # don't change this. It sets from judge node
-        self.drone_target_pose_pub.publish(pose)
+                    
+            pose = Pose()
+            pose.position.x = self.pos_x
+            pose.position.y = self.pos_y
+            pose.position.z = self.Z0   # don't change this. It sets from judge node
+            self.drone_target_pose_pub.publish(pose)
 
 
